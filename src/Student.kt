2ca607fd558622797fe9e3c08204b5b456ad40ff
RiaTiG <raker_2003@mail.ru>
2024-12-01 23:47:38 +0300

    Лаб 1.7 добавлена проверка на корректность всех полей

--- src/Student.kt
+++ src/Student.kt
@@ -1,4 +1,5 @@
 class Student {
+
     var id: Int = 0
         get() {
             return field
@@ -7,27 +8,34 @@ class Student {
             if (value > 0)
                 field = value
         }
+
     var lastName: String = ""
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkFio(value))
+                field = value
         }
+
     var firstName: String = ""
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkFio(value))
+                field = value
         }
+
     var surname: String = ""
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkFio(value))
+                field = value
         }
+
     var phone: String? = null
         get() {
             return field
@@ -36,37 +44,84 @@ class Student {
             if (checkPhone(value))
                 field = value
         }
+
     var telegram: String? = null
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkTelegram(value))
+                field = value
         }
+
     var email: String? = null
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkEmail(value))
+                field = value
         }
+
     var git: String? = null
         get() {
             return field
         }
         set(value) {
-            field = value
+            if (checkGit(value))
+                field = value
         }
 
     companion object{
         var count=0
-        val phoneReg = Regex("^\\+7\\d{10}\$|^8\\d{10}\$")
-        fun checkPhone(value: String?):Boolean{
+        val phoneReg = Regex("""^\+7\d{10}$|^8\d{10}$""")
+        val emailReg = Regex("""^[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$""")
+        val nameReg = Regex("""^[А-Яа-яA-Za-z-]+$""")
+        val telegramReg = Regex("""^@[A-Za-z0-9_]{5,}$""")
+        val gitReg = Regex("""^(https:/|http:)?(www\.)?github\.com[A-Za-z0-9_.-]+?$""")
+
+        fun checkPhone(value: String?):Boolean {
             if(phoneReg.matches(value.toString()))
                 return true
             else
                 println("Неправильный формат номера телефона")
             return false
+        }
+
+        fun checkFio(value: String):Boolean {
+            if (nameReg.matches(value)){
+                return true
+            }else {
+                println("Неправильный формат ФИО")
+                return false
+            }
+        }
+
+        fun checkTelegram(value: String?): Boolean {
+            return if (telegramReg.matches(value.toString())) {
+                true
+            } else {
+                println("Неправильный формат Telegram, имя пользователя должно начинаться с @")
+                false
+            }
+        }
+
+        fun checkEmail(value: String?): Boolean {
+            return if (emailReg.matches(value.toString())) {
+                true
+            } else {
+                println("Неправильный формат Email")
+                false
+            }
+        }
+
+        fun checkGit(value: String?): Boolean {
+            return if (gitReg.matches(value.toString())) {
+                true
+            } else {
+                println("Неправильный формат GitHub URL")
+                false
+            }
         }
     }
     init {
